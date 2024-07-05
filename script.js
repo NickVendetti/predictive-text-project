de2fc5090dbad9b217f7f121da4a69b046996e44
nickvendetti <nrvendetti@gmail.com>
2024-07-05 16:55:35 -0400

    final submission-completed

--- script.js
+++ script.js
@@ -1,107 +1,123 @@
-const input = document.querySelector("#fruit");
-const suggestions = document.querySelector(".suggestions ul");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const input = document.querySelector("#fruit");
+  const suggestions = document.querySelector(".suggestions ul");
 
-const fruit = [
-  "Apple",
-  "Apricot",
-  "Avocado 🥑",
-  "Banana",
-  "Bilberry",
-  "Blackberry",
-  "Blackcurrant",
-  "Blueberry",
-  "Boysenberry",
-  "Currant",
-  "Cherry",
-  "Coconut",
-  "Cranberry",
-  "Cucumber",
-  "Custard apple",
-  "Damson",
-  "Date",
-  "Dragonfruit",
-  "Durian",
-  "Elderberry",
-  "Feijoa",
-  "Fig",
-  "Gooseberry",
-  "Grape",
-  "Raisin",
-  "Grapefruit",
-  "Guava",
-  "Honeyberry",
-  "Huckleberry",
-  "Jabuticaba",
-  "Jackfruit",
-  "Jambul",
-  "Juniper berry",
-  "Kiwifruit",
-  "Kumquat",
-  "Lemon",
-  "Lime",
-  "Loquat",
-  "Longan",
-  "Lychee",
-  "Mango",
-  "Mangosteen",
-  "Marionberry",
-  "Melon",
-  "Cantaloupe",
-  "Honeydew",
-  "Watermelon",
-  "Miracle fruit",
-  "Mulberry",
-  "Nectarine",
-  "Nance",
-  "Olive",
-  "Orange",
-  "Clementine",
-  "Mandarine",
-  "Tangerine",
-  "Papaya",
-  "Passionfruit",
-  "Peach",
-  "Pear",
-  "Persimmon",
-  "Plantain",
-  "Plum",
-  "Pineapple",
-  "Pomegranate",
-  "Pomelo",
-  "Quince",
-  "Raspberry",
-  "Salmonberry",
-  "Rambutan",
-  "Redcurrant",
-  "Salak",
-  "Satsuma",
-  "Soursop",
-  "Star fruit",
-  "Strawberry",
-  "Tamarillo",
-  "Tamarind",
-  "Yuzu",
-];
+  const fruit = [
+    "Apple",
+    "Apricot",
+    "Avocado 🥑",
+    "Banana",
+    "Bilberry",
+    "Blackberry",
+    "Blackcurrant",
+    "Blueberry",
+    "Boysenberry",
+    "Currant",
+    "Cherry",
+    "Coconut",
+    "Cranberry",
+    "Cucumber",
+    "Custard apple",
+    "Damson",
+    "Date",
+    "Dragonfruit",
+    "Durian",
+    "Elderberry",
+    "Feijoa",
+    "Fig",
+    "Gooseberry",
+    "Grape",
+    "Raisin",
+    "Grapefruit",
+    "Guava",
+    "Honeyberry",
+    "Huckleberry",
+    "Jabuticaba",
+    "Jackfruit",
+    "Jambul",
+    "Juniper berry",
+    "Kiwifruit",
+    "Kumquat",
+    "Lemon",
+    "Lime",
+    "Loquat",
+    "Longan",
+    "Lychee",
+    "Mango",
+    "Mangosteen",
+    "Marionberry",
+    "Melon",
+    "Cantaloupe",
+    "Honeydew",
+    "Watermelon",
+    "Miracle fruit",
+    "Mulberry",
+    "Nectarine",
+    "Nance",
+    "Olive",
+    "Orange",
+    "Clementine",
+    "Mandarine",
+    "Tangerine",
+    "Papaya",
+    "Passionfruit",
+    "Peach",
+    "Pear",
+    "Persimmon",
+    "Plantain",
+    "Plum",
+    "Pineapple",
+    "Pomegranate",
+    "Pomelo",
+    "Quince",
+    "Raspberry",
+    "Salmonberry",
+    "Rambutan",
+    "Redcurrant",
+    "Salak",
+    "Satsuma",
+    "Soursop",
+    "Star fruit",
+    "Strawberry",
+    "Tamarillo",
+    "Tamarind",
+    "Yuzu",
+  ];
 
-function search(str) {
-  let results = [];
+  function search(str) {
+    let results = [];
+    const lowerStr = str.toLowerCase();
 
-  // TODO
+    results = fruit.filter((f) => f.toLowerCase().includes(lowerStr));
 
-  return results;
-}
+    return results;
+  }
 
-function searchHandler(e) {
-  // TODO
-}
+  function searchHandler(e) {
+    const inputVal = e.target.value;
+    const results = search(inputVal);
+    showSuggestions(results, inputVal);
+  }
 
-function showSuggestions(results, inputVal) {
-  // TODO
-}
+  function showSuggestions(results, inputVal) {
+    suggestions.innerHTML = "";
 
-function useSuggestion(e) {
-  // TODO
-}
+    if (results.length > 0 && inputVal !== "") {
+      results.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        suggestions.appendChild(li);
+      });
+    }
+  }
 
-input.addEventListener("keyup", searchHandler);
-suggestions.addEventListener("click", useSuggestion);
+  function useSuggestion(e) {
+    if (e.target.tagName === "LI") {
+      input.value = e.target.textContent;
+      suggestions.innerHTML = "";
+    }
+  }
+
+  input.addEventListener("keyup", searchHandler);
+  suggestions.addEventListener("click", useSuggestion);
+});
